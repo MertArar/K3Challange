4093471d05509aa1753f2f2779ffd7cdf5e5526a
MertArar <mertarar.ma@gmail.com>
2023-11-09 19:28:12 +0300

    Movement changed

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,6 +8,7 @@ public class Player : MonoBehaviour{
     
     private void Update()
     {
+        // Karakterin tuşlarla etkileşime geçmesi.
         Vector2 inputVector = new Vector2(0,0);
         
         if(Input.GetKey(KeyCode.W)){
@@ -23,10 +24,15 @@ public class Player : MonoBehaviour{
         inputVector.x = +1;
         }
         
+        // Karakterin haraket etmesi.
         inputVector = inputVector.normalized;
         
         Vector3 moveDirection = new Vector3(inputVector.x, 0f, inputVector.y);
         transform.position += moveDirection * moveSpeed * Time.deltaTime;
         
+        
+        // Karakteri döndürme.
+        float rotateSpeed = 10f;
+        transform.forward = Vector3.Slerp(transform.forward, moveDirection, Time.deltaTime * rotateSpeed);        
     }
 }
