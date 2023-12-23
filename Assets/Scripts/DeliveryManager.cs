e88d54e05ff1c27e5cb9e55ad6e454b95bc487c0
MertArar <mertarar.ma@gmail.com>
2023-12-23 03:33:54 +0300

    Sound effect added

--- Assets/Scripts/DeliveryManager.cs
+++ Assets/Scripts/DeliveryManager.cs
@@ -8,6 +8,8 @@ public class DeliveryManager : MonoBehaviour
 {
     public event EventHandler OnRecipeSpawned;
     public event EventHandler OnRecipeCompleted;
+    public event EventHandler OnRecipeSuccess;
+    public event EventHandler OnRecipeFailed;
     public static DeliveryManager Instance { get; private set; }
     
     [SerializeField] private RecipeListSO recipeListSO;
@@ -82,6 +84,7 @@ public class DeliveryManager : MonoBehaviour
                     waitingRecipeSOList.RemoveAt(i);
                     
                     OnRecipeCompleted?.Invoke(this, EventArgs.Empty);
+                    OnRecipeSuccess?.Invoke(this, EventArgs.Empty);
                     return;
                 }
             }
@@ -89,7 +92,7 @@ public class DeliveryManager : MonoBehaviour
         
         // No matches found!
         // Player did not deliver a correct recipe
-      
+        OnRecipeFailed?.Invoke(this, EventArgs.Empty);
     }
 
     public List<RecipeSO> GetWaitingRecipeSOList()
